8f2ffce06cd2ead54d4513f2111b9d35968ba63c
pit <piotr.stani@o2.pl>
2018-11-10 19:48:20 +0100

    Pierwsza wersja

---
+++ src/podstawy/Podstawy.java
@@ -0,0 +1,17 @@
+
+package podstawy;
+
+public class Podstawy {
+
+    public static void main(String[] args) {
+        
+        
+        System.out.println("TO JEST TEKST");
+        System.out.println("TO JEST TEKST 2");
+        
+        
+        
+        
+    }
+    
+}
