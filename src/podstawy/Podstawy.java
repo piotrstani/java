6c420b5d5927ef3d81a5e9d8f8c0db4f5d44660b
piotrstani <piotr.stani@o2.pl>
2018-11-10 20:35:54 +0100

    dodanie zabaw z print  \

--- src/podstawy/Podstawy.java
+++ src/podstawy/Podstawy.java
@@ -5,10 +5,12 @@ public class Podstawy {
 
     public static void main(String[] args) {
         
-        
+        /* println - łamanie lini, print - brak łamania linii */
+        /* ; - rozkaz */
         System.out.println("TO JEST TEKST");
         System.out.println("TO JEST TEKST 2");
-        
+        System.out.print("TO JEST TEKST 3");
+        System.out.print("TO JEST TEKST 4");
         
         
         
