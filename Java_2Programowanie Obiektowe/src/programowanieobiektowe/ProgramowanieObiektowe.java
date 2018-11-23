6ebd0c5bcf70aee806f3c23cfe9e759d13c91a88
piotrstani <piotr.stani@o2.pl>
2018-11-23 18:14:05 +0100

    Awaria, pobranie wersij repoytorium z github. | Dodanie projketu dot. programowania obiektowego

---
+++ Java_2Programowanie Obiektowe/src/programowanieobiektowe/ProgramowanieObiektowe.java
@@ -0,0 +1,169 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programowanieobiektowe;
+
+/**
+ *
+ * @author PiT
+ */
+public class ProgramowanieObiektowe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here      
+      
+    /* Obiekty - "pojemnik do przechowywania zmiennych i funkcij teamtycznie ze sobą związanych
+       Klasy    - foremki do tworzenia egzemplarzy obiektów
+        
+        properties  - wałaściwości np. kolor, wysokość     
+        metody      - funkcje 
+        -> teoretycznnie funkcij nie ma w JAVIE 
+        (bo ogólnie funkcje są poza klasą/ metody w klasie, 
+        a w Javie wszytko jest w klasach
+        np. włącz/wyłącz    
+    
+        np.:
+        System.out.println();
+        KLASA.wyjście.METODA...
+        
+        PRZECIĄŻANIE METOD 
+        tworzenie tych samych metod ale operujących np. na innych typach zmiennych 
+        kompilator sam wybierze 
+        
+    */
+// tworzenie nowego obiektu i rezerwacja miejsca w pamieci, mozemy się odwoływać         
+    
+ // **************KLASA MONITORY*************
+   System.out.println();
+   System.out.println("******MONITORY********");
+
+    // WEJŚCIE    
+    Monitor LG = new Monitor();
+    LG.szer =100;
+    LG.wys =0;
+    LG.nazwa="Eldżi";
+       
+    Monitor DELL = new Monitor(); 
+    DELL.szer =200;
+    DELL.wys =11100;
+    DELL.nazwa="Delł";
+    
+    
+    // UŻYCIE METODY WYPISYWANIE
+    System.out.println();
+    System.out.println ("*****Metody służące wypisywaniu****");
+            
+    System.out.println ("Szerokość: " + LG.szer + " Wysokość: " + LG.wys);       
+    LG.pokaz(LG.nazwa);
+    
+    System.out.println ("Szerokość: " + DELL.szer + " Wysokość: " + DELL.wys);  
+    DELL.pokaz(DELL.nazwa);
+    
+   
+    // UŻYCIE METODY DODAWANIE
+    System.out.println();
+    System.out.println ("*****Metody służące dodawniu****");           
+    int wynik = LG.dodaj(LG.szer, LG.wys);
+    System.out.println("Wynik dodawnia: "+ LG.nazwa+ " "  + wynik);
+    
+    // UŻYCIE METODY DODAWANIE -PRZECIĄŻENIE
+    System.out.println();
+    System.out.println ("*****Metody służące dodawniu - metoda przeciążona****");           
+    double  wynik2 = LG.dodaj(1.25, 1.5);
+    System.out.println("Wynik dodawnia: "+ LG.nazwa+ " "  + wynik2);
+    
+    
+    
+    // UŻYCIE METODY DZIELENIE
+    System.out.println();
+    System.out.println ("*****Metody służące dzielniu****"); 
+    
+    double iloraz = LG.dzielenie(LG.szer, LG.wys);
+    System.out.println("Wynik dzielenia: "+ LG.nazwa+ " "  + iloraz);
+    
+    
+    
+ // ************KLASA SAMOCHODY*****************
+    System.out.println();
+    System.out.println("******SAMOCHODY********");
+    
+    
+    Car polonez = new Car(1995,100);
+    System.out.println("Polonez " + polonez.rok + " " + polonez.szybkosc);
+   
+    Car audi = new Car();
+    System.out.println("Audi " + audi.rok + " " + audi.szybkosc);
+    }   
+}
+
+        class Monitor // klasa z dużej litery
+    {    
+         int szer; // propeties 
+         int wys;
+         String nazwa;
+         
+         void pokaz( String nazwa) 
+            //metoda z małej litery, wywłaj instrukcje z klamerki
+            // void pustka nie zwraca wyniku !!!
+            // metoda 
+         {          
+           
+            System.out.println("Nazwa: " + nazwa);
+         }
+        
+         int dodaj(int a, int b)
+         {
+              // metoda zajmująca się dodawaniem           
+             
+             return a + b;
+         }
+         
+         double dodaj(double a, double b)
+         {
+              // metoda zajmująca się dodawaniem           
+             
+             return a + b;
+         }
+         double dzielenie (int a, int b)
+         {
+            if (b==0)
+            {
+              System.out.println("NIe dziel przez zero!!!!");
+              return 0;
+            }
+            else
+                
+            return a/b;
+         }
+               
+        
+    } 
+
+        //KONSTRUKTORY - pozwala na skonfigurowanie nowo tworzonego obiektu
+        class Car
+    {       
+            // co się ma stać po wywołaniu  - konstruktory
+            
+            Car() // domsyślny jeśli nie przekażemy parametrów
+            { 
+            System.out.println("Konstruktor domyślny:");
+             rok = 1990;
+             szybkosc = 200;
+            }
+            
+            Car(int rok, int szybkosc) // jeśli przekażemy parametry
+            { 
+            System.out.println("Konstruktor z parametrami:");
+                                
+             this.rok = rok; // this używa się pzry kolizi nazw jakby użyć Car(x,y) nie potrzebne było by this
+             this.szybkosc = szybkosc;                   
+            }            
+                                
+            int rok;
+            int szybkosc;
+    }
